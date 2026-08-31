6b5766695c4d054a0c948ccb43423e6ab5b443c3
Kerem Basaran <kbasaran@users.noreply.github.com>
2026-08-31 21:02:42 +0200

    bugfix - no argument used for Settings()

--- main.py
+++ main.py
@@ -2302,7 +2302,7 @@ def main():
 
     args = parse_args(APP_DEFINITIONS)
     logger = setup_logging(args=args)
-    settings = Settings(APP_DEFINITIONS["app_name"])
+    settings = Settings()
 
     # ---- Create QApplication
     if not (app := qtw.QApplication.instance()):
